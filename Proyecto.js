be94a11a6f0d3110847e2d7bdc91053120248697
ErickHJ1 <erixkhj1@gmail.com>
2024-05-06 13:13:05 -0600

    Lo basico

--- Proyecto.js
+++ Proyecto.js
@@ -1,2 +1,21 @@
-var propina = document.getElementsByClassName('propina')
-var cuenta = document.getElementsByClassName('cuenta')
+function calcularPropina() {
+    var totalCompra = document.getElementById('cuenta').value;
+    var porcentajePropina = document.getElementById('propina').value;
+    var totalPersonas = document.getElementById('personas').value;
+
+    var porcentaje = porcentajePropina/100;
+    var resultado1 = totalCompra*porcentaje;
+    var resultado2 = resultado1/totalPersonas
+
+    if (totalCompra == "") {
+        document.getElementById('r3').innerHTML = "Introduzca los datos"
+        document.getElementById('r1').innerHTML = ":'v"
+        document.getElementById('r2').innerHTML = ":'v"
+    }
+
+    else {
+        document.getElementById('r1').innerHTML = resultado1.toFixed(2);
+        document.getElementById('r2').innerHTML = resultado2.toFixed(2)
+    }
+}
+
